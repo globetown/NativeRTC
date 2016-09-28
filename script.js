249875be85bba08aea3e7bf84ff5b9690e8566b2
Besart Hoxhaj <besartshyti@gmail.com>
2016-09-28 17:51:32 +0100

    add bin

--- script.js
+++ script.js
@@ -3,7 +3,8 @@
 const fs = require('fs');
 const exec = require('child_process').execSync;
 
-const WEBRTC_BIN_PATH = `${__dirname}/node_modules/react-native-webrtc/ios/WebRTC.framework`;
+const MODULE_BIN_PATH = `${__dirname}/node_modules/react-native-webrtc/ios/WebRTC.framework`;
+const WEBRTC_BIN_PATH = `${__dirname}/bin`;
 const ARCH_TYPES = ['i386','x86_64','armv7','arm64'];
 
 if(process.argv[2] === '--extract' || process.argv[2] === '-e'){
@@ -27,5 +28,12 @@ if(process.argv[2] === '--device' || process.argv[2] === '-d'){
   console.log(`Compiling device...`);
   exec(`lipo -o WebRTC -create WebRTC-armv7 WebRTC-arm64`,{cwd:WEBRTC_BIN_PATH});
   console.log(exec('ls -ahl | grep WebRTC',{cwd:WEBRTC_BIN_PATH}).toString().trim());
+  console.log('Done!');
+}
+
+if(process.argv[2] === '--move' || process.argv[2] === '-m'){
+  console.log(`Moving...`);
+  exec(`mv ${WEBRTC_BIN_PATH}/WebRTC ${MODULE_BIN_PATH}/WebRTC`,{cwd:WEBRTC_BIN_PATH});
+  console.log(exec('ls -ahl | grep WebRTC',{cwd:MODULE_BIN_PATH}).toString().trim());
   console.log('Done!');
 }
