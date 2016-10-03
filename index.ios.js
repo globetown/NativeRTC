46cc892c19b832b4498dfb2758a8df8a864bf12a
Besart Hoxhaj <besartshyti@gmail.com>
2016-10-03 16:03:11 +0100

    refactor

--- index.ios.js
+++ index.ios.js
@@ -5,8 +5,6 @@ import {AppRegistry,StyleSheet,Text,View,TouchableHighlight} from 'react-native'
 import WebRTC from 'react-native-webrtc';
 import genId from './utils.js';
 
-// const signalingServer = 'wss://localhost:8080';
-// 'video':{optional:[{sourceId:'com.apple.avfoundation.avcapturedevice.built-in_video:1'}]}
 const signalingServer = 'wss://webrtc-simple.herokuapp.com';
 const serverConnection = new WebSocket(signalingServer);
 const peerConnectionConfig = {'iceServers':[{'url':'stun:stun.l.google.com:19302'}]};
@@ -19,76 +17,69 @@ function componentReady () {
   uuid = genId();
   serverConnection = new WebSocket(signalingServer);
   serverConnection.onmessage = gotMessageFromServer;
-  var constraints = {audio:true,video:
-    // false
-    {optional:[{sourceId:'com.apple.avfoundation.avcapturedevice.built-in_video:1'}]}
-  };
+  var constraints = {audio:true,video:{optional:[{sourceId:'com.apple.avfoundation.avcapturedevice.built-in_video:1'}]}};
   WebRTC.getUserMedia(constraints, stream => {
     localStream = stream;
     componentInstance.setState({localVideoUrl:stream.toURL()});
-  }, error => errorHandler('error',error));
+  },console.log);
 }
 
 function start(isCaller) {
-  console.log('start:isCaller',isCaller);
   peerConnection = new WebRTC.RTCPeerConnection(peerConnectionConfig);
   peerConnection.addEventListener('icecandidate',gotIceCandidate);
   peerConnection.addEventListener('addstream',gotRemoteStream);
   peerConnection.addStream(localStream);
   if(isCaller) {
-    peerConnection.createOffer(gotDescription,errorHandler);
+    peerConnection.createOffer(gotDescription,console.log);
   }
-  console.log('start:peerConnection',peerConnection);
 }
 
 function gotDescription(description) {
-  peerConnection.setLocalDescription(
-    description,
-    () => serverConnection.send(JSON.stringify({'sdp':peerConnection.localDescription,'uuid':uuid})),
-    errorHandler
-  );
+  peerConnection.setLocalDescription(description,success,console.log);
+  function success () {
+    serverConnection.send(JSON.stringify({
+      'sdp':peerConnection.localDescription,
+      'uuid':uuid
+    }));
+  }
 }
 
 function gotIceCandidate(event) {
   if(event.candidate !== null) {
-    serverConnection.send(JSON.stringify({'ice':event.candidate,'uuid':uuid}));
+    serverConnection.send(JSON.stringify({
+      'ice':event.candidate,
+      'uuid':uuid
+    }));
   }
 }
 
 function gotRemoteStream(event) {
-  console.log('gotRemoteStream');
   componentInstance.setState({remoteVideoUrl:event.stream.toURL()});
 }
 
 function gotMessageFromServer(message) {
   var signal = JSON.parse(message.data);
   if(signal.uuid === uuid) return;
-  console.log('gotMessageFromServer:signal',signal);
   if(peerConnection === undefined) start(false);
   if(signal.sdp) {
-    peerConnection.setRemoteDescription(
-      new WebRTC.RTCSessionDescription(signal.sdp),
-      function() {
-        if(signal.sdp.type === 'offer') {
-          peerConnection.createAnswer(createdDescription,errorHandler);
-        }
-      },
-      errorHandler
-    );
+    peerConnection.setRemoteDescription(new WebRTC.RTCSessionDescription(signal.sdp),success,console.log);
   } else if(signal.ice) {
-    peerConnection.addIceCandidate(new WebRTC.RTCIceCandidate(signal.ice),errorHandler);
+    peerConnection.addIceCandidate(new WebRTC.RTCIceCandidate(signal.ice),console.log);
+  }
+  function success () {
+    if(signal.sdp.type === 'offer') {
+      peerConnection.createAnswer(createdDescription,console.log);
+    }
   }
 }
 
 function createdDescription(description) {
-  console.log('createdDescription',description);
   peerConnection.setLocalDescription(description,function() {
-    serverConnection.send(JSON.stringify({'sdp':peerConnection.localDescription,'uuid':uuid}));
-  },errorHandler);
-}
-
-function errorHandler(error) {
-  console.log(error);
+    serverConnection.send(JSON.stringify({
+      'sdp':peerConnection.localDescription,
+      'uuid':uuid
+    }));
+  },console.log);
 }
 
 class NativeRTC extends React.Component {
--- script.js
+++ script.js
@@ -7,6 +7,10 @@ const MODULE_BIN_PATH = `${__dirname}/node_modules/react-native-webrtc/ios/WebRT
 const WEBRTC_BIN_PATH = `${__dirname}/bin`;
 const ARCH_TYPES = ['i386','x86_64','armv7','arm64'];
 
+if(process.argv[2] === '--info' || process.argv[2] === '-i'){
+  console.log(exec('lipo -info WebRTC',{cwd:MODULE_BIN_PATH}).toString().trim());
+}
+
 if(process.argv[2] === '--extract' || process.argv[2] === '-e'){
   console.log(`Extracting...`);
   ARCH_TYPES.forEach(elm => {
@@ -20,14 +24,14 @@ if(process.argv[2] === '--extract' || process.argv[2] === '-e'){
 if(process.argv[2] === '--simulator' || process.argv[2] === '-s'){
   console.log(`Compiling simulator...`);
   exec(`lipo -o WebRTC -create WebRTC-x86_64 WebRTC-i386`,{cwd:WEBRTC_BIN_PATH});
-  console.log(exec('ls -ahl | grep WebRTC',{cwd:WEBRTC_BIN_PATH}).toString().trim());
+  console.log(exec('ls -ahl | grep WebRTC',{cwd:MODULE_BIN_PATH}).toString().trim());
   console.log('Done!');
 }
 
 if(process.argv[2] === '--device' || process.argv[2] === '-d'){
   console.log(`Compiling device...`);
-  exec(`lipo -o WebRTC -create WebRTC-armv7 WebRTC-arm64`,{cwd:WEBRTC_BIN_PATH});
-  console.log(exec('ls -ahl | grep WebRTC',{cwd:WEBRTC_BIN_PATH}).toString().trim());
+  exec(`lipo -o WebRTC -create WebRTC-armv7 WebRTC-arm64`,{cwd:MODULE_BIN_PATH});
+  console.log(exec('ls -ahl | grep WebRTC',{cwd:MODULE_BIN_PATH}).toString().trim());
   console.log('Done!');
 }
 
