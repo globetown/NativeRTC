513efdef05ee8615c3f074d3380d6e83c640773e
Izaak Sofer <izaakrogan@gmail.com>
2016-11-12 17:33:21 +0000

    skypify

--- ios/NativeRTC/AppDelegate.m
+++ ios/NativeRTC/AppDelegate.m
@@ -20,6 +20,8 @@
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
 
+  // jsCodeLocation = [NSURL URLWithString:@"http://192.168.2.41:8081/index.ios.bundle?platform=ios&dev=true"];
+
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"NativeRTC"
                                                initialProperties:nil
